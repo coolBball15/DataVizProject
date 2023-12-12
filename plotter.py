5bf4c11105d39a9e48a80860ff6e255b59dd1ba4
Santilopezc <Santiago.lopez@ugent.be>
2023-12-12 23:30:11 +0100

    Implemented function to get Lime explanation Image

--- plotter.py
+++ plotter.py
@@ -5,6 +5,13 @@ from PIL import Image
 import base64
 import io
 from io import BytesIO
+from lime import lime_image
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from skimage.segmentation import mark_boundaries
+
+
+
 
 def pca_decomposition(activations):
     """Return the pca decomposition of the activations."""
@@ -56,4 +63,29 @@ def get_image(image_data):
     selected_img_base64 = base64.b64encode(selected_img_byte_array.getvalue()).decode('utf-8')
 
     return f'data:image/png;base64,{selected_img_base64}'
+
+def get_LIME_image(img, model):
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+
+    #predictions = model.predict(img_array)
+    #best = np.argmax(predictions[0])
+
+    # Create a LIME explainer
+    explainer = lime_image.LimeImageExplainer()
+
+    # Define a predict function for the model
+    def predict_function(images):
+        images = preprocess_input(images)
+        return model.predict(images)
+
+    # Explain the prediction for the image using LIME
+    explanation = explainer.explain_instance(img_array[0], predict_function, top_labels=3, num_features=5)
+
+    # Visualize the LIME explanation
+    temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
+    img_boundry = mark_boundaries(temp / 2 + 0.5, mask)
+
+    return img_boundry
 
